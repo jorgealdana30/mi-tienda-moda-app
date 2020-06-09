f46d63a766fed80c0307483d6729e852969d3539
Jorge Aldana <jorge.aldana@ultertechnologies.com>
2020-06-09 18:14:10 -0500

    Adding Toolbar Component

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,41 +1,26 @@
 <template>
     <div id="app">
-        <v-toolbar color="#f06292" class="toolbar-style">
-            <v-toolbar-title class="title-toolbar">
-                {{this.customer.firstname}} {{this.customer.lastname}}
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <div class="d-flex justify-space-around">
-                <v-icon>
-                    mdi-phone
-                </v-icon>
-                <v-icon>
-                    mdi-login
-                </v-icon>
-            </div>
-
-        </v-toolbar>
+        <Toolbar/>
     </div>
 </template>
 
 <script>
+    import Toolbar from "./Toolbar";
+
     export default {
         name: "Board",
-        data(){
-            return{
+        components: {
+            Toolbar
+        },
+        data() {
+            return {
                 customer: null
             }
         },
-        mounted() {
-            if(localStorage.getItem('customer')){
-                this.customer = JSON.parse(localStorage.getItem('customer'))
-            }else{
-                this.$router.push({name: 'Login'})
-            }
-        }
+
     }
 </script>
 
 <style scoped>
-@import "../assets/styles.css";
+    @import "../assets/styles.css";
 </style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="app">
+        <v-toolbar color="#f06292" class="toolbar-style">
+            <v-toolbar-title class="title-toolbar">
+                {{customer.firstname}} {{customer.lastname}}
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <div class="d-flex justify-space-around">
+                <v-icon class="pr-5" color="white">
+                    mdi-phone
+                </v-icon>
+                <v-icon class="pr-2" color="white">
+                    mdi-login
+                </v-icon>
+            </div>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Toolbar",
+        data(){
+            return {
+                customer: null
+            }
+        },
+        mounted() {
+            if(localStorage.getItem('customer')){
+                this.customer = JSON.parse(localStorage.getItem('customer'))
+            }else{
+                this.$router.push({name: 'Login'})
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
