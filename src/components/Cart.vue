4e542b64e73bccbf5f2addff9a46725110ea9595
Jorge Aldana <jorge.aldana@ultertechnologies.com>
2020-06-11 15:07:35 -0500

    Adding panel no items in the cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,19 +1,25 @@
 <template>
     <div id="app">
-        <h1>Carrito</h1>
+        <v-card class="cardPrincipal banner mb-0 cartPanel" flat>
+            <div style="background: white; width: 100%">
+                <v-img src="../assets/empty-cart.1bfda269.png"></v-img>
+                <h3 class="montserrat text-center" style="color: #f06292">Carrito vacío, Vamos de compras!</h3>
+            </div>
+        </v-card>
     </div>
 </template>
 
 <script>
     import Toolbar from "./Toolbar";
     import BottomMenu from "./BottomMenu";
+
     export default {
         name: "Cart",
-        components:{
+        components: {
             Toolbar, BottomMenu
         },
         mounted() {
-            if (!localStorage.getItem('customer')){
+            if (!localStorage.getItem('customer')) {
                 this.$router.push({name: 'Login'})
             }
         }
