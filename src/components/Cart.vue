fe5b6a7ae9f05dde78700b28f96f66eeabecf8b6
Jorge Aldana <jorge.aldana@ultertechnologies.com>
2020-06-12 03:22:33 -0500

    Adding module to edit product

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -21,7 +21,7 @@
                             <h2 class="mt-1" style="color: #f06292">${{(items.price * items.quantity).toFixed(2)}}</h2>
                             <v-divider class="mt-2 mb-2"></v-divider>
                             <div class="d-flex justify-space-around">
-                                <v-btn icon color="blue">
+                                <v-btn icon color="blue" @click="preCart(items)">
                                     <v-icon>
                                         mdi-pencil
                                     </v-icon>
@@ -33,11 +33,48 @@
                                 </v-btn>
                             </div>
                         </v-card-text>
-                        <v-card-actions class="d-flex flex-column">
-
-                        </v-card-actions>
                     </v-card>
                 </div>
+                <v-card class="mb-2">
+                    <h2 class="montserrat d-flex justify-end">Subtotal: <span class="ml-2 mr-4" style="color: #f06292;">${{subtotal.toFixed(2)}}</span>
+                    </h2>
+                </v-card>
+                <v-dialog v-model="selectVariant" persistent>
+                    <v-card>
+                        <v-card-title class="mb-1 pb-4" style="background-color: #f06292">
+                            <v-btn icon text @click="closeClear">
+                                <v-icon color="white">
+                                    mdi-close
+                                </v-icon>
+                            </v-btn>
+                            <v-toolbar-title>
+                                <span class="white--text ml-4">Editar Producto</span>
+                            </v-toolbar-title>
+                        </v-card-title>
+                        <v-card-text>
+                            <div class="d-flex flex-column">
+                            <span class="montserrat font-weight-bold"
+                                  style="color: #f06292;">Selecciona el Tamaño</span>
+                                <div class="d-flex justify-center flex-row">
+                                    <v-select :items="variantsList" v-model="sizeSelected" v-on:change="selectQuantity">
+                                    </v-select>
+                                </div>
+                            </div>
+                            <div v-if="enableQuantity">
+                            <span class="montserrat font-weight-bold"
+                                  style="color: #f06292;">Selecciona la cantidad</span>
+                                <v-select :items="quantityList" v-model="quantitySelected" v-on:change="processProduct">
+                                </v-select>
+                            </div>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="primary" @click="saveToCart">
+                                Agregar
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
             </div>
         </v-card>
     </div>
@@ -52,10 +89,27 @@
         components: {
             Toolbar, BottomMenu
         },
+
         data() {
             return {
                 cartEmpty: true,
-                cart: []
+                cart: [],
+                selectVariant: false,
+                variantsList: [],
+                quantityList: [],
+                enableQuantity: false,
+                productSelected: null,
+                sizeSelected: '',
+                quantitySelected: '',
+            }
+        },
+        computed: {
+            subtotal() {
+                let suma = 0;
+                for (let i in this.cart) {
+                    suma = Number(suma) + Number((this.cart[i].price * this.cart[i].quantity).toFixed(2))
+                }
+                return suma
             }
         },
         mounted() {
@@ -66,19 +120,77 @@
         activated() {
             if (localStorage.getItem('cart')) {
                 this.cart = JSON.parse(localStorage.getItem('cart'));
-                this.cartEmpty = false;
+                this.cartEmpty = this.cart.length === 0;
             } else {
                 this.cartEmpty = true;
             }
         },
+        deactivated() {
+            this.closeClear()
+        },
         methods: {
+            preCart(product) {
+                console.log(product)
+                this.selectVariant = true;
+                this.productSelected = product;
+                for (let i in this.productSelected.variants) {
+                    this.variantsList.push(this.productSelected.variants[i].size);
+                }
+            },
             deleteItems(item) {
                 for (let i in this.cart) {
                     if (item.id === this.cart[i].id) {
                         this.cart.splice(i);
                     }
                 }
+                this.cartEmpty = this.cart.length === 0;
                 localStorage.setItem('cart', JSON.stringify(this.cart))
+            },
+            selectQuantity() {
+                this.enableQuantity = true;
+                for (let i in this.productSelected.variants) {
+                    if (this.sizeSelected === this.productSelected.variants[i].size) {
+                        for (let j = 1; j <= this.productSelected.variants[i].quantity; j++) {
+                            this.quantityList.push(j)
+                        }
+                    }
+                }
+            },
+            processProduct() {
+                let productTemp = this.productSelected;
+                for (let i in productTemp.variants) {
+                    if (this.sizeSelected === productTemp.variants[i].size) {
+                        productTemp.quantity = this.quantitySelected;
+                        productTemp['variant'] = productTemp.variants[i];
+                    }
+                }
+            },
+            saveToCart() {
+                if (localStorage.getItem('cart')) {
+                    let json = [];
+                    json = JSON.parse(localStorage.getItem('cart'));
+                    for (let i in json){
+                        if(json[i].id === this.productSelected.id){
+                            json.splice(i);
+                        }
+                    }
+                    json.push(this.productSelected);
+                    localStorage.setItem('cart', JSON.stringify(json));
+                } else {
+                    let json = [];
+                    json.push(this.productSelected);
+                    localStorage.setItem('cart', JSON.stringify(json));
+                }
+                this.closeClear();
+            },
+            closeClear() {
+                this.selectVariant = false;
+                this.enableQuantity = false;
+                this.quantitySelected = null;
+                this.productSelected = null;
+                this.sizeSelected = '';
+                this.quantityList = [];
+                this.variantsList = [];
             }
         }
     }
